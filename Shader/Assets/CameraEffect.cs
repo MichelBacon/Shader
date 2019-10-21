c4429ef896c7508fca9bd885691e36b8b6efbb2c
B533P-10\533 <mickaelbergeron14@gmail.com>
2019-10-21 17:04:56 -0400

    commit finale

---
+++ Shader/Assets/CameraEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraEffect : MonoBehaviour {
+
+    private Material material;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Awake ()
+    {
+        material = new Material(Shader.Find("Hidden/Shader"));
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
